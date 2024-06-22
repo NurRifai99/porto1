3c6abb9eef9bb36bc6569e84c8c40c8476e35a06
akurifai <nurrifai0103@gmail.com>
2024-06-22 21:10:38 +0700

    tambah repo porto1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./coba.html",
+    "./publik/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
